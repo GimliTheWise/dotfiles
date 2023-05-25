3b46bea0bda07890e2be6efd56b5e6794e4c27f1
Carl von Burg <carl.vonburg@gmail.com>
2023-05-25 13:12:15 +0200

    add some icons

--- .config/nvim/after/plugin/filetree.lua
+++ .config/nvim/after/plugin/filetree.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n", "<leader>ft", "<cmd>Neotree<cr>", { desc = "[F]ile[T]ree" })
-vim.keymap.set("n", "<leader>gs", "<cmd>Neotree float it_status<cr>", { desc = "[G]it[S]tatus" })
+vim.keymap.set("n", "<leader>gs", "<cmd>Neotree float git_status<cr>", { desc = "[G]it[S]tatus" })
 vim.keymap.set("n", "<leader>tt", "<cmd>Neotree toggle<cr>", { desc = "[T]oggle[T]ree" })
