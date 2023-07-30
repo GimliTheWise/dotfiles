2e853390db54b5b39164358f326e5d9c495fbd78
Carl von Burg <carl.vonburg@gmail.com>
2023-07-30 14:20:27 +0200

    add cmake shortcut

--- .config/nvim/after/plugin/map_navigation.lua
+++ .config/nvim/after/plugin/map_navigation.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = 'Explore Files' })
 vim.keymap.set('c', 'w!!', "w !sudo tee > /dev/null %<CR>:e!<CR>", { silent = true, desc = 'Write with sudo' })
+vim.keymap.set("n", "<leader>rc", [[<Cmd>CMakeRun<CR>]], { desc = 'CMakeRun' })
