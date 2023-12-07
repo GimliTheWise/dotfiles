fd2131194e2808f1361dc41cc307f8ec662efded
Carl von Burg <carl.vonburg@gmail.com>
2023-12-07 14:19:21 +0100

    rethink filetree

--- .config/nvim/lua/custom/plugins/filetree.lua
+++ .config/nvim/lua/custom/plugins/filetree.lua
@@ -1,23 +1,4 @@
-vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require('neo-tree').setup {
-      close_if_last_window = true,
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_hidden = false,
-        },
-        follow_current_file = true,
-      }
-    }
-  end,
+  "nvim-telescope/telescope-file-browser.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
 }
